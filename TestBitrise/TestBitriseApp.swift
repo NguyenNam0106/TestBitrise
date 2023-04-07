ce2d98adef3cbfffbb695a9f896a1c0c80b7f8f7
nguyennam <namnguyen@Nams-MacBook-Pro.local>
2023-04-07 17:11:49 +0700

    First commit

---
+++ TestBitrise/TestBitriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestBitriseApp.swift
+//  TestBitrise
+//
+//  Created by Nam Nguyen on 07/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestBitriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
